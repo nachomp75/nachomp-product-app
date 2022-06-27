25a1130a092fed4ddd58878bc409d71900602ca2
Nacho <nacho75xr@gmail.com>
2022-06-27 21:18:21 +0200

    Version 1.0.0-rc

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -16,7 +16,6 @@ const { Provider } = ProductContext;
 
 export interface Props {
   product: Product;
-  // children?: ReactElement | ReactElement[];
   children: (args: ProductCardHandlers) => JSX.Element;
   className?: string;
   style?: CSSProperties;
